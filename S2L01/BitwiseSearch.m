801e598819364fbafb2c5daa0b6965867894f99b
qwerfah <qwerfah@gmail.com>
2021-04-17 23:53:12 +0300

    Visualization added.

--- S2L01/BitwiseSearch.m
+++ S2L01/BitwiseSearch.m
@@ -4,18 +4,24 @@ classdef BitwiseSearch
     end
         
     methods(Static)
-        function [X, F] = Solve(a, b, f)
+        function [X, F, X_S, F_S] = Solve(a, b, f)
             arguments
                 a (1,1) double
                 b (1,1) double
-                f handle
+                f function_handle
             end
-         
+            
+            X_S = [];
+            F_S = [];
+            
             delta = (b - a) / 4.0;
             x0 = a; f0 = f(x0);
             x1 = x0; f1 = f0;
             
-            while abs(delta) > Eps
+            while abs(delta) > BitwiseSearch.Eps
+                X_S = [X_S, x0];
+                F_S = [F_S, f0];
+                
                 x0 = x1; f0 = f1;
                 x1 = x0 + delta;
                 f1 = f(x1);
--- S2L01/main.m
+++ S2L01/main.m
@@ -1,11 +1,18 @@
 method = BitwiseSearch();
 
-method.Solve(-5.0, 5.0, @f);
+f = @(x) (sin((x.^4 + x.^3 - 3 * x + 3 - 30.^(1.0/3.0)) / 2.0) + ...
+          tanh((4 * 3.^0.5 * x.^3 - 2 * x - 6 * 2.^0.5 + 1) /  ...
+               (-2 * 3.^0.5 * x.^3 + x + 3 * 2.^0.5)) + 1.2 );
 
-function F = f(x)
-    arguments
-        x (1, 1) double
-    end
-    
-    F = x * x;
-end
+X = -2:0.01:2;
+Y = f(X);
+[X0, F0, X_S, F_S] = method.Solve(-2.0, 2.0, f);
+
+figure('Units', 'normalized', 'OuterPosition', [0 0 1 1]);
+title('Метод поразрядного поиска');
+hold on;
+
+plot(X, Y, '-b','LineWidth',1.5);
+plot(X0, F0, 'or','LineWidth', 5);
+plot(X_S, F_S, 'og','LineWidth', 2);
+legend('Целевая функция', 'Точка минимума', 'Последовательность приближений');
